6c5f7e74ddc5ce1aa1cec093bc20327521200f6d
jult <j@jult.nl>
2018-03-15 13:38:37 +0100

    synced to source

---
+++ themes/forestnight.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Name: Forest Night (jult)
+ * Date: January 3, 2007
+ *
+ * @author Julius B. Thyssen
+ * @copyright &copy; 2007 Julius B. Thyssen  http://jult.net
+ * @copyright &copy; 2005-2007 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: forestnight.php,v 1.0.0.0 2007/01/03 04:24:09 jervfors Exp $
+ * @package squirrelmail
+ * @subpackage themes
+ */
+global $color;
+ $color[0]   = '#092003'; // TitleBar
+ $color[1]   = '#DDFEAB'; // Quote in mail
+ $color[2]   = '#FECD09'; // Warning/Error Messages
+ $color[3]   = '#041001'; // Left Bar Background
+ $color[4]   = '#030900'; // Normal Background
+ $color[5]   = '#102803'; // Table Headers
+ $color[6]   = '#C3CEAC'; // Text on left bar (user created folders/Purge etc.)
+ $color[7]   = '#CEEFAC'; // Links (also email-addresses)
+ $color[8]   = '#EFFEDE'; // Normal text
+ $color[9]   = '#091902'; // Darker version of #0
+ $color[10]  = '#081800'; // Darker version of #9
+ $color[11]  = '#CCDEC0'; // Special Folders color
+ $color[12]  = '#182807'; // Alternate color for message list
+ $color[13]  = '#DEFDAF'; // Color for quoted text -- > 1 quote
+ $color[14]  = '#BDEFA1'; // Color for quoted text -- >> 2 or more
+ $color[15]  = '#163011'; // Unselectable folders
+ $color[16]  = '#F0CD09'; // Highlight color
+?>
---
+++ themes/jult_dark.php
@@ -0,0 +1,20 @@
+<?php
+global $color;
+$color[0]   = '#092003'; // TitleBar
+$color[1]   = '#DDFEAB'; // Quote in mail
+$color[2]   = '#FECF10'; // Warning/Error Messages
+$color[3]   = '#041001'; // Left Bar Background
+$color[4]   = '#030900'; // Normal Background
+$color[5]   = '#102903'; // Table Headers
+$color[6]   = '#C3CEAC'; // Text on left bar (user created folders/Purge etc.)
+$color[7]   = '#CEEFAC'; // Links (also email-addresses)
+$color[8]   = '#EFFEDE'; // Normal text
+$color[9]   = '#091902'; // Darker version of #0
+$color[10]  = '#081800'; // Darker version of #9
+$color[11]  = '#CCDDC0'; // Special Folders color
+$color[12]  = '#182907'; // Alternate color for message list
+$color[13]  = '#DEFDAF'; // Color for quoted text -- > 1 quote
+$color[14]  = '#BDEFA1'; // Color for quoted text -- >> 2 or more
+$color[15]  = '#163011'; // Unselectable folders
+$color[16]  = '#F0CD09'; // Highlight color
+?>
